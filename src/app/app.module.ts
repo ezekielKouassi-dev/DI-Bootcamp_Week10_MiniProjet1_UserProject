51e2a6d4d675d93dfd6d63d0bd18ab46ff401be0
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-02-26 23:00:59 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { CardComponent } from './components/card/card.component';
+import { TableComponent } from './components/table/table.component';
+import { TitleComponent } from './components/shared/title/title.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClient ,HttpClientModule } from "@angular/common/http"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    CardComponent,
+    TableComponent,
+    TitleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
